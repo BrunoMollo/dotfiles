b3616bef9234ef8e69bf3ad826bd0b2bc0c160bf
Bruno Mollo <dia.mollo.bruno@gmail.com>
2025-05-17 23:03:35 -0300

    neo tree

---
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,35 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  cmd = { 'NvimTreeToggle', 'NvimTreeFocus' }, -- Add commands to load the plugin
+  keys = {
+    { '<leader>e', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle NvimTree' },
+  },
+  opts = {
+    view = {
+      float = {
+        enable = true,
+        quit_on_focus_loss = true,
+        open_win_config = {
+          width = 80,
+          height = 25,
+        },
+      },
+      width = 80,
+    },
+    actions = {
+      open_file = {
+        quit_on_open = true,
+      },
+    },
+    filters = {
+      dotfiles = true,
+      git_ignored = true,
+    },
+    renderer = {
+      indent_width = 4,
+      indent_markers = {
+        enable = true,
+      },
+    },
+  },
+}
