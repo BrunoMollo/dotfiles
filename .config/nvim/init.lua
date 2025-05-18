a1156c28af40c5692ec3bf256a7fee366b50a779
Bruno Mollo <dia.mollo.bruno@gmail.com>
2025-05-18 17:37:59 -0300

    phpactor, balde-formater and prettierd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -671,18 +671,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        ['phpactor'] = {
+          cmd = { 'phpactor', 'language-server' },
+          filetypes = { 'php', 'blade' },
+        },
 
         lua_ls = {
           -- cmd = { ... },
@@ -715,6 +707,7 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'phpactor',
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -741,6 +734,7 @@ require('lazy').setup({
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
     keys = {
+
       {
         '<leader>f',
         function()
@@ -768,11 +762,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        blade = { 'blade-formatter' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
