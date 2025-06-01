284ac847cdd577e4d17ca8a5d17bfd4f44724946
BrunoMollo <dia.mollo.bruno@gmail.com>
2025-06-01 00:29:41 -0300

    mv laxy code to lua/base/lazy.lua

---
+++ .config/nvim/lua/base/lazy.lua
@@ -0,0 +1,56 @@
+return {
+  setup_lazy = function()
+    -- [[ Install `lazy.nvim` plugin manager ]]
+    --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+    local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+    if not (vim.uv or vim.loop).fs_stat(lazypath) then
+      local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+      local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+      if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+      end
+    end
+
+    ---@type vim.Option
+    local rtp = vim.opt.rtp
+    rtp:prepend(lazypath)
+
+    -- [[ Configure and install plugins ]]
+    require('lazy').setup({
+      'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+
+      -- require 'kickstart.plugins.debug',
+      -- require 'kickstart.plugins.indent_line',
+      -- require 'kickstart.plugins.lint',
+      -- require 'kickstart.plugins.autopairs',
+
+      { import = 'plugins.general' },
+      { import = 'plugins.lsp' },
+      { import = 'plugins.git' },
+      { import = 'plugins.ui' },
+      { import = 'plugins.ai' },
+      { import = 'plugins.aesthetics' },
+      { import = 'plugins.laravel' },
+    }, {
+      ui = {
+        -- If you are using a Nerd Font: set icons to an empty table which will use the
+        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+        icons = vim.g.have_nerd_font and {} or {
+          cmd = '⌘',
+          config = '🛠',
+          event = '📅',
+          ft = '📂',
+          init = '⚙',
+          keys = '🗝',
+          plugin = '🔌',
+          runtime = '💻',
+          require = '🌙',
+          source = '📄',
+          start = '🚀',
+          task = '📌',
+          lazy = '💤 ',
+        },
+      },
+    })
+  end,
+}
