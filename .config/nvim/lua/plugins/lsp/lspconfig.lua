9682616b811f8394972161cad28e1d5b2f075025
BrunoMollo <dia.mollo.bruno@gmail.com>
2025-09-18 19:49:15 -0300

    rm phpactor

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -174,10 +174,6 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      ['phpactor'] = {
-        cmd = { 'phpactor', 'language-server' },
-        filetypes = { 'php', 'blade' },
-      },
 
       lua_ls = {
         -- cmd = { ... },
@@ -210,7 +206,6 @@ return {
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'phpactor',
       'stylua', -- Used to format Lua code
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
