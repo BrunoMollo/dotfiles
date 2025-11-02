ff8c9514b24d1d2476ed2308a38ea193f9f41846
BrunoMollo <dia.mollo.bruno@gmail.com>
2025-11-01 23:43:53 -0300

    rm laravel-nvim keybinds

--- .config/nvim/lua/plugins/laravel/laravel-nvim.lua
+++ .config/nvim/lua/plugins/laravel/laravel-nvim.lua
@@ -7,11 +7,6 @@ return {
     'kevinhwang91/promise-async',
   },
   cmd = { 'Laravel' },
-  keys = {
-    { '<leader>la', ':Laravel artisan<cr>' },
-    { '<leader>lr', ':Laravel routes<cr>' },
-    { '<leader>lm', ':Laravel related<cr>' },
-  },
   event = { 'VeryLazy' },
   opts = {},
   config = true,
