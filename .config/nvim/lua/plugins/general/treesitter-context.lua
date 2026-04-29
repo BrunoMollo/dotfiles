0623d7b6157b70fda31ef2bce442a00dad05cb73
BrunoMollo <dia.mollo.bruno@gmail.com>
2026-04-28 21:26:37 -0300

    add 2 nvim plugins

---
+++ .config/nvim/lua/plugins/general/treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('treesitter-context').setup {
+      max_lines = 4, -- how many stacked lines you want
+    }
+  end,
+}
